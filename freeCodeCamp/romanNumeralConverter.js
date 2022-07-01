66a5835ecd2d442667670595307f0fb3749b5927
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-01 14:32:17 +0800

    Update romanNumeralConverter.js

--- freeCodeCamp/romanNumeralConverter.js
+++ freeCodeCamp/romanNumeralConverter.js
@@ -1,3 +1,5 @@
+//b-knd (jingru) on 01 July 2022
+
 //having array variables to store roman number and values
 const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
