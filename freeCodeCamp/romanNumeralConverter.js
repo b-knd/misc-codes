d5dea5ec2e4a20b77163114c8224ed81ec4d7233
jingru <104969882+b-knd@users.noreply.github.com>
2022-07-01 09:55:34 +0800

    Create romanNumeralConverter.js

---
+++ freeCodeCamp/romanNumeralConverter.js
@@ -0,0 +1,19 @@
+//having array variables to store roman number and values
+const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+function convertToRoman(num) {
+  
+  let result = "";
+  
+  for(let i = 0; i < values.length; i++){
+    //loop through the roman array, and moving towards smaller value after appending to result (by comparing values) and deducting from the arguments
+    while(num >= values[i]){
+      num -= values[i];
+      result += roman[i];
+    }
+  }
+  return result;
+}
+
+console.log(convertToRoman(2));
